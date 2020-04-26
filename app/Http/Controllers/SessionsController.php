2c5c698c30d452baa3ad63feaa8826a70ef0bfde
Thomas <1210280984@qq.com>
2020-04-26 07:37:41 +0000

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -13,14 +13,14 @@ class SessionsController extends Controller
         return view('sessions.create');
     }
 
-     public function store(Request $request)
+    public function store(Request $request)
     {
        $credentials = $this->validate($request, [
            'email' => 'required|email|max:255',
            'password' => 'required'
        ]);
 
-       if (Auth::attempt($credentials)) {
+       if (Auth::attempt($credentials, $request->has('remember'))) {
            session()->flash('success', '欢迎回来！');
            return redirect()->route('users.show', [Auth::user()]);
        } else {
