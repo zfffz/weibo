dd9740ef2489aa14d75812c4d2b6a55e8b742583
Thomas <1210280984@qq.com>
2020-04-26 12:05:40 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store', 'index']
         ]);
 
         $this->middleware('guest', [
@@ -51,6 +51,12 @@ class UsersController extends Controller
     {
         $this->authorize('update', $user);
         return view('users.edit', compact('user'));
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
     }
 
     public function update(User $user, Request $request)
